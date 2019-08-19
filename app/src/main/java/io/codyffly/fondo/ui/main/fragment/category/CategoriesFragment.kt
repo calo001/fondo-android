2da34039397438e860d6de602e83059a52d4d0ab
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-18 22:06:59 -0500

    Redesign categories usign a GridLayoutManager

--- app/src/main/java/io/codyffly/fondo/ui/main/fragment/category/CategoriesFragment.kt
+++ app/src/main/java/io/codyffly/fondo/ui/main/fragment/category/CategoriesFragment.kt
@@ -13,28 +13,16 @@ import io.codyffly.fondo.adapter.CategoryAdapter
 import io.codyffly.fondo.repository.CategoriesRepositoryProvider
 import kotlinx.android.synthetic.main.fragment_categories.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [CategoriesFragment.newInstance] factory method to
  * create an instance of this fragment.
  *
  */
-class CategoriesFragment : androidx.fragment.app.Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CategoriesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -50,22 +38,10 @@ class CategoriesFragment : androidx.fragment.app.Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoriesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        const val TAG = "CategoriesFragment"
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            CategoriesFragment()
     }
 }
