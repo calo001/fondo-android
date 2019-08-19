7a8a459be5c1c1563389c119813677948b9d2d38
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-18 22:03:24 -0500

    Reorganize the Main Activity, fragments and functions

--- app/src/main/java/io/codyffly/fondo/ui/main/MainActivity.kt
+++ app/src/main/java/io/codyffly/fondo/ui/main/MainActivity.kt
@@ -1,71 +1,95 @@
 package io.codyffly.fondo.ui.main
 
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
 import io.codyffly.fondo.R
+import io.codyffly.fondo.dialog.SearchDialogFragment
+import io.codyffly.fondo.dialog.SearchDialogFragment.OnSearchListener
 import io.codyffly.fondo.ui.main.fragment.category.CategoriesFragment
-import io.codyffly.fondo.ui.main.fragment.photo.PhotosFragment
+import io.codyffly.fondo.ui.main.fragment.photo.*
 import io.codyffly.fondo.ui.main.fragment.search.SearchFragment
 import io.codyffly.fondo.util.makeStatusBarTransparent
-import io.codyffly.fondo.util.setMarginTop
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), PhotosFragment.OnFragmentInteractionListener{
-    val mainFragment: PhotosFragment = PhotosFragment.newInstance()
-    val categoriesFragment: CategoriesFragment = CategoriesFragment.newInstance("p", "p")
-    val searchFragment: SearchFragment = SearchFragment.newInstance()
-    lateinit var activeFragment: androidx.fragment.app.Fragment
-
-    override fun onFragmentInteraction(uri: Uri) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    fun setupFragments() {
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.mainFragment, mainFragment)
-            .add(R.id.mainFragment, searchFragment).hide(searchFragment)
-            .add(R.id.mainFragment, categoriesFragment).hide(categoriesFragment)
-            .commit()
-        activeFragment = mainFragment
-    }
+class MainActivity : AppCompatActivity(), OnSearchListener {
+    private val mainFragment: PhotosFragment =              PhotosFragment.newInstance()
+    private val categoriesFragment: CategoriesFragment =    CategoriesFragment.newInstance()
+    private val searchFragment: SearchFragment =            SearchFragment.newInstance()
+    private lateinit var activeFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        makeStatusBarTransparent()
+        setupStatusbar()
+        setSupportActionBar(toolbar)
+        setupToolbar()
+        setupFragments()
+        setupButtonNavigation()
+    }
+
+    private fun setupStatusbar() {
+        makeStatusBarTransparent("#CDF5F5F5")
         ViewCompat.setOnApplyWindowInsetsListener(mainContainer) { _, insets ->
             //mainCardView.setMarginTop(insets.systemWindowInsetTop)
             insets.consumeSystemWindowInsets()
         }
+    }
 
-        //mainAppBarLayout.setMarginTop(40)
-        setSupportActionBar(toolbar)
-        setupToolbar(getString(R.string.toolbar_title_today))
-        setupFragments()
-        setupButtonNavigation()
+    private fun setupToolbar() {
+        supportActionBar?.title = null
+        toolbar.setOnClickListener {
+            val dialog = SearchDialogFragment()
+            val ft =
+                ViewCompat.getTransitionName(toolbar)?.let { transitionName ->
+                    supportFragmentManager.beginTransaction()
+                        .addSharedElement(toolbar, transitionName)
+                }
+            dialog.show(ft, SearchDialogFragment.TAG)
+        }
+    }
+
+    private fun setupFragments() {
+        with(supportFragmentManager) {
+            val ft = beginTransaction()
+            if (supportFragmentManager.findFragmentByTag(PhotosFragment.TAG) == null) {
+                ft.add(R.id.mainFragment, mainFragment, PhotosFragment.TAG)
+                activeFragment = mainFragment
+            }
+
+            if (supportFragmentManager.findFragmentByTag(PhotosFragment.TAG) == null) {
+                ft.add(R.id.mainFragment, searchFragment, SearchFragment.TAG)
+                ft.hide(searchFragment)
+            }
+
+            if(supportFragmentManager.findFragmentByTag(PhotosFragment.TAG) == null) {
+                ft.add(R.id.mainFragment, categoriesFragment, CategoriesFragment.TAG)
+                ft.hide(categoriesFragment)
+            }
+            ft.commit()
+        }
     }
 
     private fun setupButtonNavigation() {
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.navbar_home -> {
+                    if(activeFragment == mainFragment) {
+                        mainFragment.scrollToUp()
+                    } else {
                         supportFragmentManager
                             .beginTransaction()
                             .hide(activeFragment)
                             .show(mainFragment)
                             .commit()
                         activeFragment = mainFragment
+                    }
                     }
                 R.id.navbar_categories -> {
                         supportFragmentManager
@@ -80,16 +104,14 @@ class MainActivity : AppCompatActivity(), PhotosFragment.OnFragmentInteractionLi
         }
     }
 
-    fun setupToolbar(title: String) {
-        supportActionBar?.title = null
-    }
-
-    fun toogleDayNight() {
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        }
+    override fun onSearch(term: String) {
+        Toast.makeText(this, "Query: $term", Toast.LENGTH_SHORT).show()
+        supportFragmentManager
+            .beginTransaction()
+            .hide(activeFragment)
+            .show(searchFragment)
+            .commit()
+        activeFragment = searchFragment
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -98,22 +120,10 @@ class MainActivity : AppCompatActivity(), PhotosFragment.OnFragmentInteractionLi
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_settings -> return true
-            /*R.id.action_search -> {
-            /    supportFragmentManager
-                    .beginTransaction()
-                    .hide(activeFragment)
-                    .show(searchFragment)
-                    .commit()
-                activeFragment = searchFragment
-            }*/
             else -> super.onOptionsItemSelected(item)
         }
-
         return true
     }
 }
--- app/src/main/java/io/codyffly/fondo/ui/main/fragment/photo/PhotosFragment.kt
+++ app/src/main/java/io/codyffly/fondo/ui/main/fragment/photo/PhotosFragment.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,70 +14,47 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import io.codyffly.fondo.R
 import io.codyffly.fondo.adapter.PhotosAdapter
-import io.codyffly.fondo.config.Constants
+import io.codyffly.fondo.listener.InfiniteScrollListener
+import io.codyffly.fondo.listener.InfiniteScrollListener.OnLoadMoreListener
 import io.codyffly.fondo.model.Photo
 import io.codyffly.fondo.ui.detail.PhotoDetailActivity
 import kotlinx.android.synthetic.main.fragment_photos.*
 
-class PhotosFragment : PhotoViewContract, OnItemInteraction, Fragment(){
-    private var listener: OnFragmentInteractionListener? = null
+class PhotosFragment : Fragment(), PhotoViewContract, OnItemInteraction, OnLoadMoreListener {
     private lateinit var adapter: PhotosAdapter
+    private lateinit var scrollListener: InfiniteScrollListener
 
     private val presenter: PhotosPresenterContract = PhotosPresenterImpl(this)
-    var page = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {}
-    }
+    var page = FIRST_PAGE
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_photos, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        rvTodayPhotos.layoutManager = LinearLayoutManager(activity)
+        val linearLayoutManager = LinearLayoutManager(activity)
+        scrollListener = InfiniteScrollListener(linearLayoutManager, this)
+        rvTodayPhotos.layoutManager = linearLayoutManager
+        rvTodayPhotos.addOnScrollListener(scrollListener)
 
         activity?.let {
             adapter = PhotosAdapter(mutableListOf(), it, this)
+            adapter.addHeader(resources.getString(R.string.today))
             rvTodayPhotos.adapter = adapter
-            initScrollListener()
             presenter.loadPhotos(page)
         }
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException("$context must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    private fun initScrollListener() {
-        rvTodayPhotos.addOnScrollListener(object : androidx.recyclerview.widget.RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: androidx.recyclerview.widget.RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                val layoutManager = recyclerView.layoutManager
-                if (layoutManager is LinearLayoutManager &&
-                    layoutManager.findLastCompletelyVisibleItemPosition() == (adapter.itemCount.minus(1))) {
-                    showLoading()
-                    presenter.loadPhotos(page++)
-                }
-
-            }
-        })
-    }
-
     override fun onloadPhotosSuccess(list: List<Photo>) {
         hideLoading()
+        adapter.removeNullItem()
         adapter.addPage(list)
+    }
+
+    override fun onLoadMore() {
+        page++
+        presenter.loadPhotos(page)
+        adapter.addNullItem()
     }
 
     override fun showLoading() {
@@ -87,6 +63,7 @@ class PhotosFragment : PhotoViewContract, OnItemInteraction, Fragment(){
 
     override fun hideLoading() {
         progress.visibility = View.GONE
+        scrollListener.loading = false
     }
 
     override fun showError(error: String) {
@@ -95,17 +72,7 @@ class PhotosFragment : PhotoViewContract, OnItemInteraction, Fragment(){
 
     override fun onItemInteraction(view: View, item: Photo) {
         val intent = Intent(activity, PhotoDetailActivity::class.java)
-//        intent.putExtra(Constants.EXTRA_URI_IMAGE_VIEW, item.urls.regular)
-//        intent.putExtra(Constants.EXTRA_USER_NAME, item.user.name)
-//        intent.putExtra(Constants.EXTRA_USER_BIO, item.user.bio)
-//        intent.putExtra(Constants.EXTRA_CREATED_AT, item.created_at)
-//        intent.putExtra(Constants.EXTRA_WIDTH, item.width)
-//        intent.putExtra(Constants.EXTRA_HEIGHT, item.height)
-//        intent.putExtra(Constants.EXTRA_DESCRIPTION, item.description)
-//        intent.putExtra(Constants.EXTRA_COLOR, item.color)
-//        intent.putExtra(Constants.EXTRA_DOWNLOAD_LOCATION, item.links.download_location)
-
-        intent.putExtra("object", item)
+        intent.putExtra(PhotoDetailActivity.EXTRA_OBJECT, item)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val options = ActivityOptions
@@ -116,22 +83,15 @@ class PhotosFragment : PhotoViewContract, OnItemInteraction, Fragment(){
         }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        fun onFragmentInteraction(uri: Uri)
+    fun scrollToUp() {
+        rvTodayPhotos.smoothScrollToPosition(0)
     }
 
     companion object {
+        const val TAG = "PhotosFragment"
+        const val FIRST_PAGE = 1
+
+        @JvmStatic
         fun newInstance() = PhotosFragment()
     }
 }
