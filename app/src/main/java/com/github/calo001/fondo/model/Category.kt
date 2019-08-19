413a6abfa89600ad69e76c23def04d9fca8f16c6
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-19 18:37:14 -0500

    Add query field, for send to the api

--- app/src/main/java/com/github/calo001/fondo/model/Category.kt
+++ app/src/main/java/com/github/calo001/fondo/model/Category.kt
@@ -1,3 +1,3 @@
 package com.github.calo001.fondo.model
 
-data class Category (val icon: Int, val name: Int, val background: Int)
+data class Category (val icon: Int, val name: Int, val query: String, val background: Int)
