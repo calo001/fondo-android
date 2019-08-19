363d8776a268897406e503370fad337eb4bb4730
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-19 18:34:27 -0500

    Add scroll to up function

--- app/src/main/java/com/github/calo001/fondo/ui/main/fragment/category/CategoriesFragment.kt
+++ app/src/main/java/com/github/calo001/fondo/ui/main/fragment/category/CategoriesFragment.kt
@@ -1,6 +1,7 @@
 package com.github.calo001.fondo.ui.main.fragment.category
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.github.calo001.fondo.R
 import com.github.calo001.fondo.adapter.CategoryAdapter
+import com.github.calo001.fondo.adapter.CategoryAdapter.OnCategoryInteraction
+import com.github.calo001.fondo.model.Category
 import com.github.calo001.fondo.repository.CategoriesRepositoryProvider
 import kotlinx.android.synthetic.main.fragment_categories.*
+import java.lang.RuntimeException
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +22,8 @@ import kotlinx.android.synthetic.main.fragment_categories.*
  * create an instance of this fragment.
  *
  */
-class CategoriesFragment : Fragment() {
+class CategoriesFragment : Fragment(), OnCategoryInteraction {
+    private var listener: OnCategoryListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +40,35 @@ class CategoriesFragment : Fragment() {
         rvCategories.layoutManager = GridLayoutManager(activity, 2)
         rvCategories.adapter = CategoryAdapter(
             CategoriesRepositoryProvider.provideCategoriesRepository(),
+            this,
             context
         )
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnCategoryListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnCategoryListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onCategoryInteraction(category: Category) {
+        listener?.onCategoryClick(category)
+    }
+
+    fun scrollToUp(){
+        rvCategories.smoothScrollToPosition(0)
+    }
+
+    interface OnCategoryListener {
+        fun onCategoryClick(category: Category)
     }
 
     companion object {
