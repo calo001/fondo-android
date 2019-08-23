ae3788bf08c2079dabb0455396399595d347b2a8
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-23 18:26:22 -0500

    Get application instance via companion object

--- app/src/main/java/com/github/calo001/fondo/Fondo.kt
+++ app/src/main/java/com/github/calo001/fondo/Fondo.kt
@@ -3,6 +3,15 @@ package com.github.calo001.fondo
 import android.app.Application
 
 class Fondo : Application() {
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: Fondo? = null
+        fun getInstance(): Fondo = instance as Fondo
+    }
+
     override fun onCreate() {
         super.onCreate()
     }
