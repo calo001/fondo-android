22b0dcffbc19f799450cf5150a0998fdad5e9bd1
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-23 18:30:43 -0500

    Setup recycler view for the search dialog fragment

--- app/src/main/java/com/github/calo001/fondo/ui/dialog/search/SearchDialogFragment.kt
+++ app/src/main/java/com/github/calo001/fondo/ui/dialog/search/SearchDialogFragment.kt
@@ -1,4 +1,4 @@
-package com.github.calo001.fondo.dialog
+package com.github.calo001.fondo.ui.dialog.search
 
 import android.content.Context
 import android.os.Build
@@ -8,15 +8,24 @@ import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.calo001.fondo.R
+import com.github.calo001.fondo.adapter.SearchHistoryAdapter
+import com.github.calo001.fondo.adapter.SearchHistoryAdapter.OnSearchHistoryClickListener
+import com.github.calo001.fondo.model.SearchHistory
 import kotlinx.android.synthetic.main.layout_search.*
 import kotlinx.android.synthetic.main.layout_search.view.*
 
-class SearchDialogFragment: DialogFragment() {
+class SearchDialogFragment: DialogFragment(), SearchDialogViewContract, OnSearchHistoryClickListener {
     private var listener: OnSearchListener? = null
 
+    private var presenter: SearchDialogPresenterContract = SearchDialogPresenterImpl(this)
+    private lateinit var recycler: RecyclerView
+    private lateinit var adapter: SearchHistoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
@@ -34,6 +43,8 @@ class SearchDialogFragment: DialogFragment() {
 
         view.toolbarSearch.setNavigationIcon(R.drawable.ic_back)
         view.toolbarSearch.setNavigationOnClickListener { dismiss() }
+        recycler = view.rvSearchHistory
+        presenter.getHistory()
 
         return view
     }
@@ -42,12 +53,12 @@ class SearchDialogFragment: DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let { adapter.filter(it) }
                 return false
             }
 
             override fun onQueryTextSubmit(query: String?): Boolean {
-                query?.let { listener?.onSearch(it) }
-                this@SearchDialogFragment.dismiss()
+                query?.let { sendForSearch(it) }
                 return false
             }
         })
@@ -74,6 +85,36 @@ class SearchDialogFragment: DialogFragment() {
     override fun onDetach() {
         super.onDetach()
         listener = null
+    }
+
+    override fun onSearchHistoryClick(value: String) {
+        sendForSearch(value)
+    }
+
+    override fun onDeleteItem(item: String) {
+        presenter.deleteFromHistory(item)
+    }
+
+    private fun initRecycler(list: List<String>) {
+        activity?.let {
+            adapter = SearchHistoryAdapter(list as MutableList<String>, it, this)
+            recycler.layoutManager = LinearLayoutManager(it)
+            recycler.adapter = adapter
+        }
+    }
+
+    override fun onSuccess(list: List<String>) {
+        initRecycler(list)
+    }
+
+    override fun onError(error: String) {
+        Toast.makeText(activity, error, Toast.LENGTH_LONG).show()
+    }
+
+    fun sendForSearch(value: String) {
+        listener?.onSearch(value)
+        presenter.addSearchHistoryItem(SearchHistory(value = value))
+        dismiss()
     }
 
     companion object {
