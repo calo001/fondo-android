c66ff216588a7ee606d1ce2adfbf9558df74bc63
Carlos Lopez <calo_lrc@hotmail.com>
2019-08-29 10:09:21 -0500

    Fix Block UI when a download is running

--- app/src/main/java/com/github/calo001/fondo/util/FondoDownloadManager.kt
+++ app/src/main/java/com/github/calo001/fondo/util/FondoDownloadManager.kt
@@ -2,9 +2,15 @@ package com.github.calo001.fondo.util
 
 import android.util.Log
 import com.github.calo001.fondo.repository.UnsplashRepository
+import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import okhttp3.ResponseBody
+import okio.Okio
+import okio.Okio.sink
+import retrofit2.Response
 import java.io.BufferedInputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -22,55 +28,63 @@ class FondoDownloadManager(private val listener: DownloadListener) {
 
     private fun download(url: String, fileName: String, externalDirectory: String) {
         UnsplashRepository.downloadImage(url)
-            .subscribe(object: Observer<ResponseBody> {
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe!")
-                    //d.dispose()
-                }
-
-                override fun onNext(response: ResponseBody) {
-                    try {
-                        saveImage(response, externalDirectory, fileName)
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.d(TAG, e.localizedMessage)
-                }
-
-                override fun onComplete() {
-                    Log.d(TAG, "onComplete")
-                }
-
-            })
+            .flatMap {
+                saveImage(it, externalDirectory, fileName)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+            }
+            .doOnComplete {
+                listener.onDownloadComplete(File(externalDirectory, fileName))
+            }
+            .subscribe()
     }
 
     @Throws(IOException::class)
-    private fun saveImage(body: ResponseBody, externalDirectory: String, fileName: String) {
-        var count: Int
-        val data = ByteArray(1024 * 4)
-        val fileSize = body.contentLength()
-        val inputStream = BufferedInputStream(body.byteStream(), 1024 * 8)
-        val outputFile = File(externalDirectory, fileName)
-        val outputStream = FileOutputStream(outputFile)
-        var total: Long = 0
-
-        count = inputStream.read(data)
-        while (count != -1) {
-            total += count.toLong()
-            mProgress = ((total * 100).toDouble() / fileSize.toDouble()).toInt()
-            listener.onProgressChange(mProgress)
-            outputStream.write(data, 0, count)
+    private fun saveImage(body: ResponseBody, externalDirectory: String, fileName: String): Observable<File> {
+        return Observable.create {
+            var count: Int
+            val data = ByteArray(1024 * 4)
+            val fileSize = body.contentLength()
+            val inputStream = BufferedInputStream(body.byteStream(), 1024 * 8)
+            val outputFile = File(externalDirectory, fileName)
+            val outputStream = FileOutputStream(outputFile)
+            var total: Long = 0
 
             count = inputStream.read(data)
+            while (count != -1) {
+                total += count.toLong()
+                mProgress = ((total * 100).toDouble() / fileSize.toDouble()).toInt()
+                listener.onProgressChange(mProgress)
+                outputStream.write(data, 0, count)
+
+                count = inputStream.read(data)
+
+                it.onNext(outputFile)
+            }
+
+            outputStream.flush()
+            outputStream.close()
+            inputStream.close()
+
+            it.onComplete()
         }
 
-        outputStream.flush()
-        outputStream.close()
-        inputStream.close()
-        listener.onDownloadComplete(outputFile)
+            //listener.onDownloadComplete(outputFile)
+        /*return Observable.create {
+            try {
+                val destinationFile = File(externalDirectory, fileName)
+                val bufferedSink = Okio.buffer(sink(destinationFile))
+                bufferedSink.writeAll(response.source())
+                bufferedSink.close()
+
+                it.onNext(destinationFile)
+                it.onComplete()
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+                it.onError(e)
+            }
+        }*/
     }
 
     companion object {
