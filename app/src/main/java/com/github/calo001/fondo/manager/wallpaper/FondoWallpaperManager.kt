29119ef1fcf5ef0575b7dd11ba5ce13098777b9e
Carlos Lopez <calo_lrc@hotmail.com>
2019-09-12 22:12:14 -0500

    Improve compatibility of set wallpaper intent

--- app/src/main/java/com/github/calo001/fondo/manager/wallpaper/FondoWallpaperManager.kt
+++ app/src/main/java/com/github/calo001/fondo/manager/wallpaper/FondoWallpaperManager.kt
@@ -3,32 +3,35 @@ package com.github.calo001.fondo.manager.wallpaper
 import android.app.WallpaperManager
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Build
-import android.util.Log
-import com.github.calo001.fondo.service.NotificationService
+import com.github.calo001.fondo.Fondo
 import com.github.calo001.fondo.util.getContentUri
-import java.io.IOException
 
 class FondoWallpaperManager(val context: Context, val path: String) {
-    private val wallpaperManager = WallpaperManager.getInstance(context)
-
     fun setWallpaper() {
-        try {
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
-                val uri = getContentUri(context, path)
-                val wallIntent = wallpaperManager
-                    .getCropAndSetWallpaperIntent(uri)
-                wallIntent.setDataAndType(uri, "image/*")
-                wallIntent.putExtra("mimeType", "image/*")
-                wallIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                context.startActivity(wallIntent)
-            } else {
-                wallpaperManager.setBitmap(BitmapFactory.decodeFile(path))
+        val uri = getContentUri(context, path)
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            try {
+                val wallpaperIntent =
+                    WallpaperManager.getInstance(context).getCropAndSetWallpaperIntent(uri)
+                wallpaperIntent.setDataAndType(uri, "image/*")
+                wallpaperIntent.putExtra("mimeType", "image/*")
+                context.startActivity(wallpaperIntent)
+            } catch (e: Exception) {
+                val wallpaperIntent = Intent(Intent.ACTION_ATTACH_DATA)
+                wallpaperIntent.setDataAndType(uri, "image/*")
+                wallpaperIntent.putExtra("mimeType", "image/*")
+                wallpaperIntent.addCategory(Intent.CATEGORY_DEFAULT)
+                wallpaperIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                wallpaperIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                wallpaperIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                val chooserIntent = Intent.createChooser(
+                    wallpaperIntent,
+                    "Set as wallpaper"
+                )
+                chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(chooserIntent)
             }
-
-        } catch (e: IOException) {
-            Log.d(NotificationService.TAG, e.message)
         }
     }
 }
